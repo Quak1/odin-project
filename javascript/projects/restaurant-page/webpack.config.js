51ed2aeae28ef34975561a521eb32e1fe94df977
Aldo Camacho <aldo@aldocamacho.com>
2024-08-12 11:49:49 -0700

    install gh-pages

--- javascript/projects/restaurant-page/webpack.config.js
+++ javascript/projects/restaurant-page/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "../../../docs/5-restaurant-page"),
+    path: path.resolve(__dirname, "./dist"),
     clean: true,
   },
   module: {
