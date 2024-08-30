e36e674abcff7b19ae8ec048f9179bb6f9c5d83b
Aldo Camacho <aldo@aldocamacho.com>
2024-08-30 14:01:28 -0700

    Change ship hitting logic

--- javascript/projects/battleship/src/Ship.js
+++ javascript/projects/battleship/src/Ship.js
@@ -1,19 +1,23 @@
 class Ship {
-  hits = 0;
+  hits = new Set();
   #sunk = false;
 
   constructor(size) {
     this.length = size;
   }
 
-  hit() {
+  hit(location) {
     if (this.#sunk) return;
-    this.hits++;
+    if (location >= this.length)
+      throw new Error("Can't attack ship on location larger than ship");
+    else if (this.hits.has(location))
+      throw new Error("Can't attack ship on same location twice");
+    else this.hits.add(location);
     this.isSunk();
   }
 
   isSunk() {
-    if (this.hits === this.length) this.#sunk = true;
+    if (this.hits.size === this.length) this.#sunk = true;
     return this.#sunk;
   }
 }
--- javascript/projects/battleship/src/Ship.test.js
+++ javascript/projects/battleship/src/Ship.test.js
@@ -3,30 +3,32 @@ import Ship from "./Ship";
 test("Ship is created correctly", () => {
   const ship = new Ship(4);
   expect(ship.length).toBe(4);
-  expect(ship.hits).toBe(0);
+  expect(ship.hits.size).toBe(0);
 });
 
 test("Ship can receive hits", () => {
   const ship = new Ship(4);
-  expect(ship.hits).toBe(0);
-  ship.hit();
-  expect(ship.hits).toBe(1);
+  expect(ship.hits.size).toBe(0);
+  ship.hit(0);
+  expect(ship.hits.size).toBe(1);
+  expect(ship.hits.has(0)).toBe(true);
 });
 
-test("Ship can't have more hits than its size", () => {
-  const ship = new Ship(2);
-  expect(ship.hits).toBe(0);
-  ship.hit();
-  ship.hit();
-  ship.hit();
-  expect(ship.hits).toBe(2);
+test("Ship can't receive hits out of bounds", () => {
+  const ship = new Ship(4);
+  expect(() => ship.hit(4)).toThrow();
 });
 
-test("Ship can't have more hits than its size", () => {
+test("Ship can't receive attacks on the same place", () => {
+  const ship = new Ship(4);
+  ship.hit(0);
+  expect(() => ship.hit(0)).toThrow();
+});
+
+test("Ship can't get sunk after getting hit on all locations", () => {
   const ship = new Ship(2);
-  expect(ship.hits).toBe(0);
   expect(ship.isSunk()).toBe(false);
-  ship.hit();
-  ship.hit();
+  ship.hit(0);
+  ship.hit(1);
   expect(ship.isSunk()).toBe(true);
 });
