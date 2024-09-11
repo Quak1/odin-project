39e5292802ae11f98d29fab93d6d9795e7a9130c
Aldo Camacho <aldo@aldocamacho.com>
2024-09-11 15:54:04 -0700

    fetch card information from amiibo api

--- react/memory-card/src/App.jsx
+++ react/memory-card/src/App.jsx
@@ -5,8 +5,6 @@ import CardContainer from "./components/CardContainer";
 import GameOver from "./components/GameOver";
 import "./assets/app.css";
 
-import data from "./assets/example.json";
-
 const size = 12;
 const shuffleArray = (array) => {
   return array.slice().sort(() => 0.5 - Math.random());
@@ -20,13 +18,23 @@ function App() {
   const score = clickedCards.length;
 
   useEffect(() => {
-    const cards = data.amiibo.map((card) => ({
-      id: card.head + "-" + card.tail,
-      name: card.name,
-      image: card.image,
-    }));
-    setCards(cards);
-    setActiveCards(shuffleArray(cards).slice(0, size));
+    fetch("https://www.amiiboapi.com/api/amiibo/?type=figure")
+      .then((res) => res.json())
+      .then((data) =>
+        data.amiibo.map((card) => ({
+          id: card.head + "-" + card.tail,
+          name: card.name,
+          image: card.image,
+        })),
+      )
+      .then((cards) => {
+        setCards(cards);
+        setActiveCards(shuffleArray(cards).slice(0, size));
+      })
+      .catch((e) => {
+        console.log(e);
+        setCards("fail");
+      });
   }, []);
 
   const shuffleActiveCards = () => {
@@ -54,12 +62,20 @@ function App() {
     shuffleActiveCards();
   };
 
-  if (!activeCards) return <div>Loading...</div>;
-
   return (
     <>
       <Header changeAmiibos={changeAmiibos} score={score} />
-      <CardContainer cards={activeCards} onCardClick={onCardClick} />
+      {cards === "fail" ? (
+        <h2>
+          There was an error getting the cards information.
+          <br />
+          The Amiibo API might be down.
+        </h2>
+      ) : !activeCards ? (
+        <div>Loading...</div>
+      ) : (
+        <CardContainer cards={activeCards} onCardClick={onCardClick} />
+      )}
       {isGameOver && (
         <GameOver score={score} size={size} handleNewGame={handleNewGame} />
       )}
