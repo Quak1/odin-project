93a3ee6861f05140bb8b65026cb2792733f607db
Aldo Camacho <aldo@aldocamacho.com>
2024-09-19 13:05:24 -0700

    change vit config for developtment

--- react/shopping-cart/vite.config.js
+++ react/shopping-cart/vite.config.js
@@ -2,7 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/shopping-cart",
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  if (command === "serve") {
+    return {
+      plugins: [react()],
+    };
+  } else
+    return {
+      base: "/shopping-cart",
+      plugins: [react()],
+    };
 });
