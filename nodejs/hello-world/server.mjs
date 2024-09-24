6d67a9740666622c6476ba0f1c8adba355466f15
Aldo Camacho <aldo@aldocamacho.com>
2024-09-24 14:25:18 -0700

    read search parameters

--- nodejs/hello-world/server.mjs
+++ nodejs/hello-world/server.mjs
@@ -1,4 +1,5 @@
 import { createServer } from "node:http";
+import { URL } from "node:url";
 
 const hostname = "127.0.0.1";
 const port = 3000;
@@ -6,7 +7,12 @@ const port = 3000;
 const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+  res.write(req.url);
+
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  url.searchParams.forEach((value, key) => res.write(`\n${key}: ${value}`));
+
+  res.end("\nHello World");
 });
 
 server.listen(port, hostname, () => {
