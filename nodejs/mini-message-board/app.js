c421298dc133faa5f62432f14b08870c9ffba1b5
Aldo Camacho <aldo@aldocamacho.com>
2024-09-26 14:55:51 -0700

    remove unnecessary 'path' require

--- nodejs/mini-message-board/app.js
+++ nodejs/mini-message-board/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const path = require("path");
 
 const errorController = require("./controllers/errorController");
 const indexRouter = require("./routes/indexRouter");
