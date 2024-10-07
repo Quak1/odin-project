3745482465cd7906b2f293affefe953228e40bc3
Aldo Camacho <aldo@aldocamacho.com>
2024-10-07 13:54:35 -0700

    add books form data validation

--- nodejs/inventory-application/controllers/booksController.js
+++ nodejs/inventory-application/controllers/booksController.js
@@ -1,6 +1,53 @@
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+
 const queries = require("../db/queries");
 const NotFoundError = require("../errors/NotFoundError");
+
+const currentYear = new Date().getFullYear();
+const bookValidator = [
+  (req, res, next) => {
+    for (const key in req.body) {
+      if (req.body[key] === "") req.body[key] = null;
+    }
+    next();
+  },
+  body("title")
+    .trim()
+    .isLength({ min: 1, max: 500 })
+    .withMessage("Title must be between 1 and 500 characters"),
+  body("author")
+    .trim()
+    .isLength({ min: 1, max: 100 })
+    .withMessage("Author must be between 1 and 100 characters"),
+  body("year")
+    .optional({ values: "null" })
+    .trim()
+    .isInt({ min: 2000, max: currentYear })
+    .withMessage(`Year must be a number between 2000 and ${currentYear}`),
+  body("rating")
+    .optional({ values: "null" })
+    .trim()
+    .isFloat({ min: 0, max: 5 })
+    .withMessage("Rating must be a number between 0 and 5"),
+  body("pages")
+    .optional({ values: "null" })
+    .trim()
+    .isInt({ min: 1 })
+    .withMessage("Pages must be a whole positive number"),
+  body("cover_s")
+    .optional({ values: "null" })
+    .trim()
+    .isURL()
+    .withMessage("Cover small must be a valid URL"),
+  body("cover_l")
+    .optional({ values: "null" })
+    .trim()
+    .isURL()
+    .withMessage("Cover large must be a valid URL"),
+  body("description").optional({ values: "null" }).trim(),
+  body("genre").toArray(),
+];
 
 const pageParser = require("../middleware/pageParser");
 const PAGE_SIZE = process.env.BOOKS_PAGE_SIZE;
@@ -25,7 +72,7 @@ const getAllBooks = [
     const genres = await queries.getAllGenres();
 
     const books = genre
-      ? await queries.getBooksByGenre(genre)
+      ? await queries.getBooksByGenre(genre, page.current)
       : await queries.getAllBooks(page.current);
 
     const title = genre ? `${genre} Books` : "Books";
@@ -35,44 +82,77 @@ const getAllBooks = [
 ];
 
 const getBook = asyncHandler(async (req, res) => {
-  const bookId = req.params.id;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) throw new NotFoundError();
 
+  const bookId = req.params.id;
   const book = await queries.getBookById(bookId);
   if (!book) throw new NotFoundError();
   res.render("book", { title: `Book | ${book.title}`, book });
 });
 
-const getCreateBook = asyncHandler(async (req, res) => {
+const renderEditBook = async (res, locals) => {
   const genres = await queries.getAllGenres();
-  res.render("editBook", { title: "Create book", genres });
+  res.render("editBook", {
+    title: "Create book",
+    genres,
+    ...locals,
+  });
+};
+
+const getCreateBook = asyncHandler(async (req, res) => {
+  await renderEditBook(res);
 });
 
-const postCreateBook = asyncHandler(async (req, res) => {
-  const book = req.body;
-  const id = await queries.createBook(book);
-  res.redirect("/books/" + id);
-});
+const postCreateBook = [
+  bookValidator,
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      await renderEditBook(res, {
+        formData: req.body,
+        errors: errors.mapped(),
+      });
+      return;
+    }
+
+    const book = req.body;
+    const id = await queries.createBook(book);
+    res.redirect("/books/" + id);
+  }),
+];
 
 const getEditBook = asyncHandler(async (req, res) => {
-  const bookId = req.params.id;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) throw new NotFoundError();
 
+  const bookId = req.params.id;
   const book = await queries.getBookById(bookId);
+  if (!book) throw new NotFoundError();
   const genres = await queries.getAllGenres();
-  res.render("editBook", { title: "Edit book", book, genres });
+  res.render("editBook", { title: "Edit book", formData: book, genres });
 });
 
-const postEditBook = asyncHandler(async (req, res) => {
-  const bookId = req.params.id;
+const postEditBook = [
+  bookValidator,
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      await renderEditBook(res, {
+        formData: req.body,
+        errors: errors.mapped(),
+      });
+    }
 
-  const book = req.body;
-  book.id = bookId;
-  await queries.updateBook(book);
-  res.redirect("/books/" + bookId);
-});
+    const book = req.body;
+    book.id = req.params.id;
+    await queries.updateBook(book);
+    res.redirect("/books/" + book.id);
+  }),
+];
 
 const deleteBook = asyncHandler(async (req, res) => {
   const bookId = req.params.id;
-
   await queries.deleteBook(bookId);
   res.status(204).send();
 });
--- nodejs/inventory-application/routes/booksRouter.js
+++ nodejs/inventory-application/routes/booksRouter.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controllers/booksController");
 
-const idParser = require("../middleware/idParser");
+const controller = require("../controllers/booksController");
+const idValidator = require("../middleware/idValidator");
 
 router.get("/", controller.getAllBooks);
 
 router.get("/create", controller.getCreateBook);
 router.post("/create", controller.postCreateBook);
 
-router.get("/:id/edit", idParser, controller.getEditBook);
-router.post("/:id/edit", idParser, controller.postEditBook);
+router.get("/:id/edit", idValidator, controller.getEditBook);
+router.post("/:id/edit", idValidator, controller.postEditBook);
 
-router.delete("/:id", idParser, controller.deleteBook);
-router.get("/:id", idParser, controller.getBook);
+router.delete("/:id", idValidator, controller.deleteBook);
+router.get("/:id", idValidator, controller.getBook);
 
 module.exports = router;
