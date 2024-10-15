b328746c9a793aec40cc2aa5759ec06d3032c249
Aldo Camacho <aldo@aldocamacho.com>
2024-10-15 14:12:00 -0700

    handle multer errors in file post route

--- nodejs/file-uploader/controllers/errorController.js
+++ nodejs/file-uploader/controllers/errorController.js
@@ -1,7 +1,10 @@
 const NotFoundError = require("../errors/NotFoundError");
 
 const error = (error, req, res, next) => {
-  if (error.statusCode !== 404) console.error(error);
+  if (!error.statusCode) {
+    console.error(error);
+    error.message = "There was an error!";
+  }
   if (error.code === "ECONNREFUSED") {
     error.message = "The database seems to be down, please come back later.";
     error.statusCode = 503;
@@ -16,7 +19,19 @@ const notFound = (req, res, next) => {
   throw new NotFoundError();
 };
 
+const multerError = (error, req, res, next) => {
+  if (error.code === "LIMIT_FILE_SIZE")
+    req.flash("errors", "File exceeds size limit");
+  else {
+    console.error(error);
+    req.flash("errors", "There was an error uploading the file");
+  }
+
+  res.redirect("/file");
+};
+
 module.exports = {
   error,
   notFound,
+  multerError,
 };
