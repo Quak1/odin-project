029b3fd0d34c2e8b93fe0b319d95b2909dae1fb8
Aldo Camacho <aldo@aldocamacho.com>
2024-10-29 12:39:38 -0700

    add user verification to user routes

--- nodejs/blog-api/backend/routes/usersRouter.js
+++ nodejs/blog-api/backend/routes/usersRouter.js
@@ -1,17 +1,17 @@
 const express = require("express");
 
 const controller = require("../controllers/usersController");
-const authController = require("../controllers/auth");
+const { authorized, login } = require("../controllers/auth");
 const router = express.Router();
 
 router.post("/", controller.createUser);
 router.get("/", controller.getAllUsers);
-router.post("/login", authController.login);
+router.post("/login", login);
 router.get("/:id", controller.getUserById);
-router.get("/:id/posts", controller.getUserPosts);
+router.get("/:id/posts", authorized, controller.getUserPosts);
 router.get("/:id/posts/public", controller.getUserPostsPublic);
-router.put("/:id/username", controller.updateUsername);
-router.put("/:id/password", controller.updatePassword);
-router.delete("/:id", controller.deleteUser);
+router.put("/:id/username", authorized, controller.updateUsername);
+router.put("/:id/password", authorized, controller.updatePassword);
+router.delete("/:id", authorized, controller.deleteUser);
 
 module.exports = router;
