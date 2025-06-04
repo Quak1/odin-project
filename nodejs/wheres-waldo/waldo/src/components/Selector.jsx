9255628ffc2ea469c3722811a0365f1b7786d568
Aldo Camacho <me@aldocamacho.com>
2025-06-04 15:33:39 -0700

    prefenvt selector from going out of viewport

--- nodejs/wheres-waldo/waldo/src/components/Selector.jsx
+++ nodejs/wheres-waldo/waldo/src/components/Selector.jsx
@@ -1,8 +1,24 @@
+import { useRef, useState, useLayoutEffect } from "react";
+
 import styles from "./styles/Selector.module.css";
 
 const Selector = ({ characters, pos, onClick }) => {
+  const selectorRef = useRef(null);
+  const [position, setPosition] = useState(pos);
+
+  useLayoutEffect(() => {
+    let top = pos.top;
+    let left = pos.left;
+
+    const selector = selectorRef.current.getBoundingClientRect();
+    if (selector.bottom > window.innerHeight) top -= selector.height;
+    if (selector.right > window.innerWidth) left -= selector.width;
+
+    setPosition({ top, left });
+  }, []);
+
   return (
-    <div className={styles.selector} style={pos}>
+    <div className={styles.selector} style={position} ref={selectorRef}>
       {characters.map(
         (char) =>
           !char.found && (
