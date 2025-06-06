cd83f81052b682a1f5b20a382e22a97aaeba94e6
Aldo Camacho <me@aldocamacho.com>
2025-06-05 19:40:29 -0700

    move toast launcher to utils

--- nodejs/wheres-waldo/waldo/src/utils.js
+++ nodejs/wheres-waldo/waldo/src/utils.js
@@ -1,4 +1,7 @@
+import { toast, Slide } from "react-toastify";
+
 import { API_URL } from "./config/constants";
+import style from "./components/styles/Toast.module.css";
 
 export const fetchData = async (resource) => {
   const res = await fetch(`${API_URL}/${resource}`, { credentials: "include" });
@@ -14,7 +17,8 @@ export const postData = async (resource, data) => {
     credentials: "include",
   });
 
-  return await res.json();
+  if (res.ok) return await res.json();
+  else return { error: "There was an error submitting your score." };
 };
 
 export const formatMinutes = (millis) => {
@@ -34,3 +38,25 @@ export const formatDate = (d) => {
 };
 
 const formatTimeComponent = (t) => (t < 10 ? `0${t}` : t);
+
+export const notify = (msg, type = "success") => {
+  toast(msg, {
+    position: "bottom-right",
+    autoClose: 5000,
+    hideProgressBar: true,
+    closeOnClick: true,
+    transition: Slide,
+    className: style.toast,
+    type: type,
+  });
+};
+
+export const notifyFatal = (msg, type = "error") => {
+  toast(msg, {
+    hideProgressBar: true,
+    autoClose: false,
+    transition: Slide,
+    theme: "colored",
+    type: type,
+  });
+};
