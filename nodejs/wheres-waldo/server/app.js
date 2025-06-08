c88c0f653f780b2fb98ed957ab59a50845c00892
Aldo Camacho <me@aldocamacho.com>
2025-06-08 15:50:39 -0700

    change cookie config

--- nodejs/wheres-waldo/server/app.js
+++ nodejs/wheres-waldo/server/app.js
@@ -20,5 +20,5 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}, ${process.env.NODE_ENV}`);
 });
